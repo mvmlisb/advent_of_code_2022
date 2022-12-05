80258918ec52e5e90b7afc1baf8775d008b05aa7
Mihail Mozgovoy <mihail.mozgovoy@atola.com>
2022-12-05 13:03:05 +0200

    In progress

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,25 +1,33 @@
 fun main() {
-    val input = readInput("Day05_test")
-    val lineIndexWithCrateStackNumbers = input.indexOfFirst { it.matches("^[\\s,1-9]*\$".toRegex()) }
-
-    val part1 = ProcessInstructions(
-        input,
-        lineIndexWithCrateStackNumbers,
-        CreateStacks(input, lineIndexWithCrateStackNumbers),
-        false
-    )
-
-    val part2 = ProcessInstructions(
-        input,
-        lineIndexWithCrateStackNumbers,
-        CreateStacks(input, lineIndexWithCrateStackNumbers),
-        true
-    )
+    val part1 = Solve(false)
+    val part2 = Solve(true)
 
     println(part1)
     println(part2)
 }
 
+private fun Solve(reverseFirstCratesInTargetStackAfterAdd: Boolean): String {
+    val input = readInput("Day05_test")
+    val lineIndexWithCrateStackNumbers = input.indexOfFirst { it.matches("^[\\s,1-9]*\$".toRegex()) }
+
+    val stacks = CreateStacks(input, lineIndexWithCrateStackNumbers)
+
+    input.drop(lineIndexWithCrateStackNumbers + 2).forEach { line ->
+        val count = line.substringAfter("move").substringBefore("from").TrimedInt()
+        val from = line.substringAfter("from").substringBefore("to").TrimedInt()
+        val to = line.substringAfter("to").TrimedInt()
+
+        val targetStack = stacks[to - 1]
+        repeat(count) {
+            targetStack.addFirst(stacks[from - 1].removeFirst())
+        }
+
+        if (reverseFirstCratesInTargetStackAfterAdd)
+            targetStack.ReverseFirst(count)
+    }
+
+    return stacks.joinToString("") { it.first().trim('[', ']') }
+}
 
 private fun CreateStacks(input: List<String>, lineIndexWithCrateStackNumbers: Int): List<ArrayDeque<String>> {
     val stackCrateCount = input[lineIndexWithCrateStackNumbers].split("   ").size
@@ -37,42 +45,16 @@ private fun CreateStacks(input: List<String>, lineIndexWithCrateStackNumbers: In
     return stacks
 }
 
-private fun ProcessInstructions(
-    input: List<String>,
-    lineIndexWithCrateStackNumbers: Int,
-    stacks: List<ArrayDeque<String>>,
-    reverseFirstCratesInTargetStackAfterAdd: Boolean
-): String {
-    input.drop(lineIndexWithCrateStackNumbers + 2).forEach { line ->
-        val count = line.substringAfter("move").substringBefore("from").TrimedInt()
-        val from = line.substringAfter("from").substringBefore("to").TrimedInt()
-        val to = line.substringAfter("to").TrimedInt()
-
-        val targetStack = stacks[to - 1]
-        repeat(count) {
-            targetStack.addFirst(stacks[from - 1].removeFirst())
-        }
-
-        if (reverseFirstCratesInTargetStackAfterAdd)
-            targetStack.ReverseFirst(count)
-    }
-
-    return stacks.map(ArrayDeque<String>::first).joinToString("") { it.substring(1..1) }
-}
-
-private fun String.TrimedInt() = this.trim().toInt()
+private fun String.TrimedInt() = trim().toInt()
 
 private fun ArrayDeque<String>.ReverseFirst(count: Int) {
     var startIndex = 0
-    var endIndex = count - 1
-    while (startIndex < endIndex) {
+    while (startIndex < count / 2) {
         val temp = this[startIndex]
 
-        this[startIndex] = this[endIndex]
-        this[endIndex] = temp
+        this[startIndex] = this[count - startIndex - 1]
+        this[count - startIndex - 1] = temp
 
         startIndex++
-        endIndex--
     }
 }
-
