0c45c3ce9cc9a68392b7f61c9965c55e80b86406
Mihail Mozgovoy <mihail.mozgovoy@atola.com>
2022-12-05 12:51:10 +0200

    In progress

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,14 @@
 private val Priorities = (('a'.code..'z'.code) + ('A'.code..'Z'.code))
     .mapIndexed { charIndex, charCode -> charCode to charIndex + 1 }.toMap()
 
-private fun FindCommonChar(strings: List<String>): Char {
-    val firstString = strings.first()
-    val restStrings = strings.subList(1, strings.size)
-    return firstString.first { char -> restStrings.all { string -> string.contains(char) } }
-}
+private fun FindCommonChar(strings: List<String>) =
+    strings.first().first { char -> strings.drop(1).all { string -> string.contains(char) } }
 
 fun main() {
     val input = readInput("Day03_test")
 
     val part1 = input.sumOf {
-        val commonChar = FindCommonChar(it.chunked(it.length / 2))
-        Priorities[commonChar.code] ?: 0
+        Priorities[FindCommonChar(it.chunked(it.length / 2)).code] ?: 0
     }
 
     val part2 = input.chunked(3).sumOf {
